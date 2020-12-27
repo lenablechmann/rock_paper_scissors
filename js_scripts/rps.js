a08a78fee075d097b0d2701b1f6b2437e5c12b06
Lena Blechmann <lenablechmann@gmail.com>
2020-12-27 09:59:48 +0100

    Write pseudocode into js, get user input

--- js_scripts/rps.js
+++ js_scripts/rps.js
@@ -1,3 +1,21 @@
-function some_function() {
-    console.log("Hello there.")
+function user_play(){
+    // get user input via prompt function user_play
+    let user_text = prompt("Enter either 'rock' 'paper' or 'scissors'");
+
+    console.log(`User chose ${user_text}.`);
+
+    // Parse user input into 0(rock), 1(paper), 2 (scissors), case not sensitive
+    // return that number
 }
+
+// write a function computer_play
+// that randomly selects rock, paper, scissors for the computer.
+// this computerPlay function will return its result as an int
+
+
+// write a function that plays a single round of RPS playRound()
+// which will take 2 parameters: player_selection and computer_selection
+// it shall return a string like "You Lose! Paper beats Rock."
+
+// write another function, that will play 5 rounds, 
+// keeps score and reports the overall winner at the end
